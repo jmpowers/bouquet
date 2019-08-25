df3dd897fe13ff877ca77ec36df400c79d2b4ec1
John Powers <icyjava@gmail.com>
2019-08-25 11:15:57 -0700

    don't use base function names

--- R/filter_RT.R
+++ R/filter_RT.R
@@ -1,17 +1,17 @@
 #' Filter on retention times
 #'
-#' This function allows you to filter out compounds that elute before a minimum retention time and/or after a maximum retention time.
+#' This function allows you to filter out compounds that elute before a RT_minimum retention time and/or after a RT_maximum retention time.
 #' @param chemtable the data frame of the data about the compounds
-#' @param min the minimum retention time
-#' @param max the maximum retention time
+#' @param RT_min the RT_minimum retention time
+#' @param RT_max the RT_maximum retention time
 #' @keywords retention time filter
 #' @examples
 #' data(GCMSfloral)
 #' chemtable <- filter_RT(chemtable, 4, 15)
 #' @export
-filter_RT<-function(chemtable, min, max){
-  attr(chemtable, "RT_min") <- min
-  attr(chemtable, "RT_max") <- max
+filter_RT<-function(chemtable, RT_min, RT_max){
+  attr(chemtable, "RT_min") <- RT_min
+  attr(chemtable, "RT_max") <- RT_max
   return(within(chemtable,
-         filter_RT <- factor(ifelse(RT <= max & RT >= min , "OK", ifelse(RT >= max, "High", "Low")))))
+         filter_RT <- factor(ifelse(RT <= RT_max & RT >= RT_min , "OK", ifelse(RT >= RT_max, "High", "Low")))))
 }
